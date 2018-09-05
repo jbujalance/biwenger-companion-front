a90a4b8b2d9d3395783a0ff43ae24bb865cd638f
Jose Bujalance <joseanbu23@gmail.com>
2018-09-05 14:07:23 +0200

    Add auth header to balances service

--- src/app/services/balances.service.ts
+++ src/app/services/balances.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IUserBalance } from '../model/user-balance';
 import { map } from 'rxjs/operators';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,23 @@ export class BalancesService {
 
   private readonly url: string = 'https://biwengertransfers.herokuapp.com/api/balances';
   private readonly initialCapital: number = 20_000_000;
+  private httpOptions: object;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthenticationService) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': authService.getTokenHeader()
+      })
+    };
+  }
 
   getBalances(): Observable<IUserBalance[]> {
-    return this.http.get<IUserBalance[]>(this.url)
-      .pipe (map((userBalances: IUserBalance[]) => {
+    return this.http.get<IUserBalance[]>(this.url, this.httpOptions)
+    .pipe(
+      map((userBalances: IUserBalance[]) => {
         userBalances.forEach((userBalance: IUserBalance) => userBalance.balance += this.initialCapital);
         return userBalances;
-      }));
+      })
+    );
   }
 }
