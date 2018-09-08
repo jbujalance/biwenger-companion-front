b52778db60516da62604e54a06c240a90b2ff618
Jose Bujalance <joseanbu23@gmail.com>
2018-09-08 11:04:02 +0200

    Login form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { ILoginRegisterPayload } from '../../model/login-register-payload';
 import { LoginRegisterService } from '../../services/login-register.service';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,10 @@ import { AuthenticationService } from '../../services/authentication.service';
 })
 export class LoginComponent implements OnInit {
 
-  public credentials: ILoginRegisterPayload = {
-    email: '',
-    password: ''
-  }
+  public loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
 
   constructor(private authService: AuthenticationService, private loginService: LoginRegisterService, private router: Router) { }
 
@@ -25,11 +26,26 @@ export class LoginComponent implements OnInit {
   }
 
   login(): void {
-    this.loginService.login(this.credentials).subscribe(() => {
+    this.loginService.login(this.formToPayload()).subscribe(() => {
       this.router.navigateByUrl('/payments');
     }, err => {
       console.error(err);
     });
+  }
+
+  get email(): AbstractControl {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.get('password');
+  }
+
+  private formToPayload(): ILoginRegisterPayload {
+    return {
+      email: this.email.value,
+      password: this.password.value
+    }
   }
 
 }
