1c4b81fcbee0bc1e18f375c9a0973b98b26925e8
Jose Bujalance <joseanbu23@gmail.com>
2018-09-09 11:19:29 +0200

    Remove useless import

--- src/app/interceptors/unauthorized.interceptor.ts
+++ src/app/interceptors/unauthorized.interceptor.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from "@angular/common/http";
 import { AlertService } from "../services/alert.service";
 import { Observable, of } from "rxjs";
-import { tap, catchError } from "rxjs/operators";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
