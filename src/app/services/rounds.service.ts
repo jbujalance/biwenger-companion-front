4b89f39a1dac6d4a34e0365938877ef457eb03f7
Jose Bujalance <joseanbu23@gmail.com>
2018-09-16 12:36:31 +0200

    Create round service

---
+++ src/app/services/rounds.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IRound } from '../model/round';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoundsService {
+
+  private readonly url: string = environment.apiUrl + 'rounds';
+
+  constructor(private http: HttpClient) { }
+
+  public getRounds(): Observable<IRound[]> {
+    return this.http.get<IRound[]>(this.url);
+  }
+}
