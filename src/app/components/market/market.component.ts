36666394b17b9e1e3f4f2430d41bf7dc58f17da8
Jose Bujalance <joseanbu23@gmail.com>
2018-11-11 19:40:06 +0100

    Market component impl

--- src/app/components/market/market.component.ts
+++ src/app/components/market/market.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IMarketSale } from 'src/app/model/market-sale';
+import { MarketService } from 'src/app/services/market.service';
 
 @Component({
   selector: 'app-market',
   templateUrl: './market.component.html',
   styleUrls: ['./market.component.css']
 })
-export class MarketComponent implements OnInit {
+export class MarketComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public marketSales: IMarketSale[];
+  private marketSubscription: Subscription;
+
+  constructor(private marketService: MarketService) { }
 
   ngOnInit() {
+    this.getMarketSales();
+  }
+
+  getMarketSales(): void {
+    this.marketSubscription = this.marketService.getMarketSales()
+      .subscribe((data: IMarketSale[]) => this.marketSales = data);
+  }
+
+  ngOnDestroy() {
+    this.marketSubscription.unsubscribe();
   }
 
 }
