cdcc36afc5283d536e7a47f7fe80cb718cbde62a
Jose Bujalance <joseanbu23@gmail.com>
2019-08-17 17:29:01 +0200

    Payment components subscribe to selected season

--- src/app/components/payments/rounds/rounds.component.ts
+++ src/app/components/payments/rounds/rounds.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IRound } from '../../../model/round';
 import { Subscription } from 'rxjs';
 import { RoundsService } from '../../../services/rounds.service';
+import { ISeason } from 'src/app/model/season';
+import { SeasonsService } from 'src/app/services/seasons.service';
 
 @Component({
   selector: 'app-rounds',
@@ -11,21 +13,25 @@ import { RoundsService } from '../../../services/rounds.service';
 export class RoundsComponent implements OnInit, OnDestroy {
 
   public rounds: IRound[];
-  private subscription: Subscription;
+  private roundsSubscription: Subscription;
+  private seasonSubscription: Subscription;
 
-  constructor(private roundsService: RoundsService) { }
+  constructor(private roundsService: RoundsService, private seasonService: SeasonsService) { }
 
   ngOnInit() {
-    this.getRounds();
+    this.seasonSubscription = this.seasonService.getSelectedSeason().subscribe((season: ISeason) => {
+      this.getRounds(season);
+    });
   }
 
-  private getRounds(): void {
-    this.subscription = this.roundsService.getRounds()
+  private getRounds(season: ISeason): void {
+    this.roundsSubscription = this.roundsService.getRounds(season)
       .subscribe((data: IRound[]) => this.rounds = data);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.roundsSubscription.unsubscribe();
+    this.seasonSubscription.unsubscribe();
   }
 
 }
