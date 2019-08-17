1a9ecea1d570008202326d162f3052907f94c938
Jose Bujalance <joseanbu23@gmail.com>
2019-08-17 17:28:37 +0200

    SeasonService holds the selected season

--- src/app/services/seasons.service.ts
+++ src/app/services/seasons.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { ISeason } from '../model/season';
 
 @Injectable({
@@ -9,12 +9,28 @@ import { ISeason } from '../model/season';
 })
 export class SeasonsService {
 
+  private static DEFAULT_SEASON: ISeason = {
+    _id: "0",
+    key: 2019,
+    name: "2019/2020"
+  }
+  private selectedSeason: BehaviorSubject<ISeason>;
   private readonly url: string = environment.apiUrl + 'seasons';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.selectedSeason = new BehaviorSubject<ISeason>(SeasonsService.DEFAULT_SEASON);
+  }
 
   getSeasons(): Observable<ISeason[]> {
     return this.http.get<ISeason[]>(this.url);
+  }
+
+  getSelectedSeason(): Observable<ISeason> {
+    return this.selectedSeason;
+  }
+
+  setSelectedSeason(season: ISeason) {
+    this.selectedSeason.next(season);
   }
 
   getCurrentSeason(): Observable<ISeason> {
